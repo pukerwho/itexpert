bc164174c98dfb20d44feec260be4deab797b33f
Vladimir Samojlov <v-moi@ya.ru>
2021-01-13 17:10:19 +0200

    edit

--- functions.php
+++ functions.php
@@ -44,9 +44,10 @@ function crb_load() {
 add_action( 'carbon_fields_register_fields', 'crb_register_custom_fields' );
 function crb_register_custom_fields() {
   require_once __DIR__ . '/inc/custom-fields/gutenberg-blocks.php';
+  require_once get_template_directory() . '/inc/custom-fields/category-meta.php';
 }
 
-require_once get_template_directory() . '/inc/custom-fields/category-meta.php';
+
 
 register_nav_menus( array(
   'head_menu' => 'Меню в шапке',
